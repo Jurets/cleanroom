89f77469cf53c3a70d802e6b4a6c5a0645c06d06
Jurets <jurets75@gmail.com>
2013-12-03 12:36:52 +0200

    есть две вьюхи, для главной и для внутренних.. Продублируйте пункты меню и номеР телефона с главной на вторую

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -39,7 +39,7 @@
             <form class="search">
 
 				<?php echo CHtml::form($this->createUrl('/store/category/search')) ?>
-					<input type="text" value="Поиск товаров" name="q" id="searchQuery">
+                    <input type="text" placeholder="Поиск товаров" name="q" id="searchQuery">
 					<input type="submit" value="">
 				<?php echo CHtml::endForm() ?>
 
@@ -52,7 +52,8 @@
             </div>
 
             <div class="hd_pad1">
-            	<p class="tel">8 800 795-62-90<br />8 800 795-62-90</p>
+                <!--<p class="tel">8 800 795-62-90<br />8 800 795-62-90</p>-->
+            	<p class="tel">+7 (926) 336-08-12<br><br></p>
                 <p class="worktime">Работаем с 9:00  до 21:00  без выходных</p>
             </div>
 
@@ -61,10 +62,14 @@
 				$this->widget('zii.widgets.CMenu2', array(
 					'items'=>array(
                         array('label'=>Yii::t('core', 'Главная'), 'url'=>Yii::app()->createAbsoluteUrl()),
-						array('label'=>Yii::t('core', 'Помощь'), 'url'=>array('/pages/pages/view', 'url'=>'help'),),
+						/*array('label'=>Yii::t('core', 'Помощь'), 'url'=>array('/pages/pages/view', 'url'=>'help'),),
 						array('label'=>Yii::t('core', 'Как сделать заказ'), 'url'=>array('/pages/pages/view', 'url'=>'how-to-create-order')),
 						array('label'=>Yii::t('core', 'Гарантия'), 'url'=>array('/pages/pages/view', 'url'=>'garantiya')),
-						array('label'=>Yii::t('core', 'Доставка и оплата'), 'url'=>array('/pages/pages/view', 'url'=>'dostavka-i-oplata')),
+						array('label'=>Yii::t('core', 'Доставка и оплата'), 'url'=>array('/pages/pages/view', 'url'=>'dostavka-i-oplata')),*/
+                        array('label'=>Yii::t('core', 'О компании'), 'url'=>array('/pages/pages/view', 'url'=>'about')),
+                        array('label'=>Yii::t('core', 'Сервис'), 'url'=>array('/pages/pages/view', 'url'=>'service')),
+                        array('label'=>Yii::t('core', 'Оплата и доставка'), 'url'=>array('/pages/pages/view', 'url'=>'dostavka-i-oplata')),
+                        array('label'=>Yii::t('core', 'Контакты'), 'url'=>array('/pages/pages/view', 'url'=>'contacts')),
 					),
 				));
 			?>
