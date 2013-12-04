96b5f5dfec62c2c2bb2a3ab48e4dee0fb4caeced
Jurets <jurets75@gmail.com>
2013-12-04 11:53:36 +0200

    ремонт Поиска

--- themes/default/views/store/category/search.php
+++ themes/default/views/store/category/search.php
@@ -29,43 +29,71 @@ $this->breadcrumbs[] = Yii::t('StoreModule.core', 'Поиск');
 		?></h1>
 
 		<div class="actions">
-			<?php
-				echo Yii::t('StoreModule.core', 'Сортировать:');
-				echo CHtml::dropDownList('sorter', Yii::app()->request->url, array(
-					Yii::app()->request->removeUrlParam('/store/category/search', 'sort')  => '---',
-                        Yii::app()->request->addUrlParam('/store/category/search', array('sort'=>'price'))  => Yii::t('StoreModule.core', 'Сначала дешевые'),
-					Yii::app()->request->addUrlParam('/store/category/search', array('sort'=>'price.desc')) => Yii::t('StoreModule.core', 'Сначала дорогие'),
-				), array('onchange'=>'applyCategorySorter(this)'));
-			?>
+            <div class="filter">
+                <div class="line">
+                    <div class="sort">
+                        <label for="">Сортировка: </label>
+			            <?php
+				            //echo Yii::t('StoreModule.core', 'Сортировать:');
+				            echo CHtml::dropDownList('sorter', Yii::app()->request->url, array(
+					            Yii::app()->request->removeUrlParam('/store/category/search', 'sort')  => '---',
+                                    Yii::app()->request->addUrlParam('/store/category/search', array('sort'=>'price'))  => Yii::t('StoreModule.core', 'Сначала дешевые'),
+					            Yii::app()->request->addUrlParam('/store/category/search', array('sort'=>'price.desc')) => Yii::t('StoreModule.core', 'Сначала дорогие'),
+				            ), array(
+                                'onchange'=>'applyCategorySorter(this)',
+                                //'id'=>'sorter',
+                                'class'=>'sm'
+                            ));
+			            ?>
+                    </div>
+                    <div class="sort">
+                        <label for="">На странице: </label>
+			            <?php
+				            $limits=array(Yii::app()->request->removeUrlParam('/store/category/search', 'per_page')  => $this->allowedPageLimit[0]);
+				            array_shift($this->allowedPageLimit);
+				            foreach($this->allowedPageLimit as $l)
+					            $limits[Yii::app()->request->addUrlParam('/store/category/search', array('per_page'=> $l))]=$l;
 
-			<?php
-				$limits=array(Yii::app()->request->removeUrlParam('/store/category/search', 'per_page')  => $this->allowedPageLimit[0]);
-				array_shift($this->allowedPageLimit);
-				foreach($this->allowedPageLimit as $l)
-					$limits[Yii::app()->request->addUrlParam('/store/category/search', array('per_page'=> $l))]=$l;
-
-				echo Yii::t('StoreModule.core', 'На странице:');
-				echo CHtml::dropDownList('per_page', Yii::app()->request->url, $limits, array('onchange'=>'applyCategorySorter(this)'));
-			?>
-		</div>
-
-		<?php
-			if(isset($provider))
-			{
-				$this->widget('zii.widgets.CListView', array(
-					'dataProvider'=>$provider,
-					'ajaxUpdate'=>false,
-					'template'=>'{items} {pager} {summary}',
-					'itemView'=>'_product',
-					'sortableAttributes'=>array(
-						'name', 'price'
-					),
-				));
-			}
-			else
-			{
-				echo Yii::t('StoreModule.core', 'Нет результатов');
-			}
-		?>
+				            //echo Yii::t('StoreModule.core', 'На странице:');
+				            echo CHtml::dropDownList('per_page', Yii::app()->request->url, $limits, array(
+                                'onchange'=>'applyCategorySorter(this)',
+                                'class'=>'sm',
+                            ));
+			            ?>
+                    </div>
+                </div>
+		    </div>
+        </div>
+        <script type="">
+             function foo(url){
+                 window.location = url;
+             }
+             function applyCategorySorter(el) {
+                 window.location = $(el).val();
+             }
+        </script>
+        
+        
+        <div class="item_list">
+		    <?php
+			    if(isset($provider))
+			    {
+				    $this->widget('zii.widgets.CListView', array(
+					    'dataProvider'=>$provider,
+					    'ajaxUpdate'=>false,
+					    'template'=>'{items} {pager} {summary}',
+					    'itemView'=>'_product',
+					    'sortableAttributes'=>array(
+						    'name', 'price'
+					    ),
+				    ));
+			    }
+			    else
+			    {
+				    echo Yii::t('StoreModule.core', 'Нет результатов');
+			    }
+		    ?>
+        </div>
+        
 	</div>
 </div>
